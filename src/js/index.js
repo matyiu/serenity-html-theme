1b8a1021ed342d2102d1a43e7d94b390b2bdbfa6
Jefferson Valle <valleespinozajefferson@gmail.com>
2021-11-08 12:54:39 +0100

    Added responsive menu functionality

--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,29 @@ const initImages = (images) => {
     });
 }
 
+const initResponsiveMenu = () => {
+    const menu = document.querySelector('.menu.menu-responsive');
+    const menuButton = document.querySelector('.menu-toggle');
+
+    const toggleBtn = () => {
+        const open = menu.dataset.open;
+
+        menu.dataset.open = open === 'true' ? 'false' : 'true';
+    }
+
+    const closeMenu = (e) => {
+        if (menu.dataset.open === 'true' && (e.target !== menuButton && !menuButton.contains(e.target))) {
+            menu.dataset.open = 'false';
+        }
+
+        if (e.target.tagName.toLowerCase() === 'a') {
+            e.target.click();
+        }
+    }
+
+    menuButton.addEventListener('click', toggleBtn);
+    window.addEventListener('mousedown', closeMenu);
+}
+
+initResponsiveMenu();
 initPortfolio();
